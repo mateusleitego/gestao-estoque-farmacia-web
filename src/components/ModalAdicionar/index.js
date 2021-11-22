6e4ba17f2a970177c42ac0dec1a800d259ca353e
mateusleitego <82689003+mateusleitego@users.noreply.github.com>
2021-11-22 11:26:41 -0300

    Add files via upload

--- src/components/ModalAdicionar/index.js
+++ src/components/ModalAdicionar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Button, Modal } from 'react-bootstrap';
 
